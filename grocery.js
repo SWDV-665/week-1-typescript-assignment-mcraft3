c7d5f36d2bca3e5fde99184a42e7cbfe20e3f40f
Mike Craft <mcraft3@live.maryville.edu>
2022-03-10 00:54:15 -0500

    adds for loop to reduce code

--- grocery.js
+++ grocery.js
@@ -7,7 +7,7 @@ var Grocery = /** @class */ (function () {
         this.quantity = quantity;
     }
     Grocery.prototype.getGroceryProperties = function () {
-        return "".concat(this.id, " ").concat(this.name, " ").concat(this.size, " ").concat(this.ui, " ").concat(this.quantity);
+        return "".concat(this.id, ", ").concat(this.name, ", ").concat(this.size, ", ").concat(this.ui, ", ").concat(this.quantity);
     };
     return Grocery;
 }());
@@ -16,15 +16,10 @@ var bread = new Grocery(2, "Bread", "King", "Loaf", 6);
 var eggs = new Grocery(3, "Eggs", "X-Large", "Dozen", 11);
 var groceries = [];
 groceries.push(milk, bread, eggs);
-var message1 = (groceries[0].id + ', ' + groceries[0].name + ', ' + groceries[0].size + ', ' + groceries[0].ui + ', ' + groceries[0].quantity);
-var heading1 = document.createElement('h3');
-heading1.textContent = message1;
-document.body.appendChild(heading1);
-var message2 = (groceries[1].id + ', ' + groceries[1].name + ', ' + groceries[1].size + ', ' + groceries[1].ui + ', ' + groceries[1].quantity);
-var heading2 = document.createElement('h3');
-heading2.textContent = message2;
-document.body.appendChild(heading2);
-var message3 = (groceries[2].id + ', ' + groceries[2].name + ', ' + groceries[2].size + ', ' + groceries[2].ui + ', ' + groceries[2].quantity);
-var heading3 = document.createElement('h3');
-heading3.textContent = message3;
-document.body.appendChild(heading3);
+for (var x in groceries) {
+    var message = groceries[x].getGroceryProperties();
+    var heading = document.createElement('h3');
+    heading.textContent = message;
+    document.body.appendChild(heading);
+}
+;
--- grocery.ts
+++ grocery.ts
@@ -22,32 +22,21 @@ class Grocery {
   }
   
   getGroceryProperties() {
-    return `${this.id} ${this.name} ${this.size} ${this.ui} ${this.quantity}`;
+    return `${this.id}, ${this.name}, ${this.size}, ${this.ui}, ${this.quantity}`;
   }
 
 }
 
 let milk = new Grocery(1, "Milk", "Medium", "Gallon", 3);
-
 let bread = new Grocery(2, "Bread", "King", "Loaf", 6);
-
 let eggs = new Grocery(3, "Eggs", "X-Large", "Dozen", 11);
 
 let groceries = [];
 groceries.push(milk, bread, eggs);
 
-
-let message1: string = (groceries[0].id + ', ' + groceries[0].name + ', ' + groceries[0].size + ', ' + groceries[0].ui + ', ' + groceries[0].quantity);
-let heading1 = document.createElement('h3');
-heading1.textContent = message1;
-document.body.appendChild(heading1);
-
-let message2: string = (groceries[1].id + ', ' + groceries[1].name + ', ' + groceries[1].size + ', ' + groceries[1].ui + ', ' + groceries[1].quantity);
-let heading2 = document.createElement('h3');
-heading2.textContent = message2;
-document.body.appendChild(heading2);
-
-let message3: string = (groceries[2].id + ', ' + groceries[2].name + ', ' + groceries[2].size + ', ' + groceries[2].ui + ', ' + groceries[2].quantity);
-let heading3 = document.createElement('h3');
-heading3.textContent = message3;
-document.body.appendChild(heading3);
+for (let x in groceries) {
+  let message = groceries[x].getGroceryProperties();
+  let heading = document.createElement('h3');
+  heading.textContent = message;
+  document.body.appendChild(heading);
+};
